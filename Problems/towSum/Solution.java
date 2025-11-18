bfaa5b1a3c8738082180e993909931783f3f371c
Your Name <you@example.com>
2025-11-18 10:35:55 -0500

    ejercicios leet code

---
+++ Problems/towSum/Solution.java
@@ -0,0 +1,20 @@
+package towSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNum = nums[i];
+            int complement = target - currentNum;
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(currentNum, i);
+        }
+        throw new IllegalArgumentException("No se encontró una solución Two Sum válida.");
+    }
+}
